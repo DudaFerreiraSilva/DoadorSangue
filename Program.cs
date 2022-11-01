fbe3e4991a880de69ad742c26ecb83cb0f1cffe3
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 19:41:56 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int idade;
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Qual a sua idade: ");
+Console.ResetColor();
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você pode ser doador de sangue.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Você não pode ser doador de sangue.");
+    Console.ResetColor();
+}
